f88c353c60ade4ae1be82d72def515e994bb2946
Baptiste <baptiste.grosr@gmail.com>
2024-03-21 11:11:52 +1100

    git init

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss']
+})
+export class Tab4Page {
+  // map?: L.Map;
+  //
+  // constructor() {}
+  //
+  // ionViewDidEnter() {
+  //   this.map = L.map('map').setView([51.505, -0.09], 13);
+  //   L.tileLayer('https://tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png', {
+  //     attribution:
+  //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  //   }).addTo(this.map);
+  //   const markPoint = L.marker([51.5, -0.09]).addTo(this.map);
+  //   markPoint.bindPopup("<p>Ici c'est Londres</p>");
+  //   this.map.addLayer(markPoint);
+  // }
+
+}
